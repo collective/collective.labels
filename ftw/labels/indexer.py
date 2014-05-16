d368f932c4d3e1f25e8675a4f2da0cc040a0b640
Jonas Baumann <jone@jone.ch>
2014-05-16 09:49:06 +0200

    Pyflakes cleanup.

--- ftw/labels/indexer.py
+++ ftw/labels/indexer.py
@@ -2,7 +2,6 @@ from ftw.labels.interfaces import ILabelSupport
 from ftw.labels.interfaces import ILabeling
 from operator import itemgetter
 from plone.indexer.decorator import indexer
-from zope.interface import Interface
 
 
 @indexer(ILabelSupport)
