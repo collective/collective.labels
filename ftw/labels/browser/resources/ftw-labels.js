f4d08a35a2f994d0f6278c3dd96e756da7a5236d
Jonas Baumann <jone@jone.ch>
2014-05-16 12:34:55 +0200

    Javascript: add missing semicolons.

--- ftw/labels/browser/resources/ftw-labels.js
+++ ftw/labels/browser/resources/ftw-labels.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 
     $('.colorBox').click(function() {
         // reset all selected
-        $(this).closest('form').find('.colorBox').removeClass('selected')
+        $(this).closest('form').find('.colorBox').removeClass('selected');
         $(this).toggleClass('selected');
-        $(this).closest('form').find('input[name=color]').val($(this).attr("data-color"))
+        $(this).closest('form').find('input[name=color]').val($(this).attr("data-color"));
      });
 
     $('.labelItem').click(function() {
